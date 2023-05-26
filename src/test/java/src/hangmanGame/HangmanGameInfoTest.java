e8b0894f028ede1585976d23690fe29e13bca59c
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-05-27 01:45:43 +0900

    test: (step1) HangmanGameInfo 검증 테스트 코드 생성

---
+++ src/test/java/src/hangmanGame/HangmanGameInfoTest.java
@@ -0,0 +1,39 @@
+package src.hangmanGame;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class HangmanGameInfoTest {
+
+    @Test
+    void validateNumberGames_예외_테스트() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new HangmanGameInfo("wrong value", "2");
+        });
+    }
+
+    @Test
+    void validateNumberGames_예외_메시지_테스트() {
+        try {
+            new HangmanGameInfo("wrong value", "2");
+        } catch (IllegalArgumentException exception) {
+            Assertions.assertEquals("올바른 게임 횟수를 입력해 주세요.", exception.getMessage());
+        }
+    }
+
+    @Test
+    void validateLife_예외_테스트() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new HangmanGameInfo("2", "wrong value");
+        });
+    }
+
+    @Test
+    void validateLife_예외_메시지_테스트() {
+        try {
+            new HangmanGameInfo("2", "wrong value");
+        } catch (IllegalArgumentException exception) {
+            Assertions.assertEquals("올바른 목숨을 입력해 주세요.", exception.getMessage());
+        }
+    }
+}
