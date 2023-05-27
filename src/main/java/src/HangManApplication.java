072d305cc2a318211ed33d238ffc9126ee322230
sdfgx123 <sdfgx123@naver.com>
2023-05-27 12:38:21 +0900

    Feat: firstly designed project and initiation

--- src/main/java/src/HangManApplication.java
+++ src/main/java/src/HangManApplication.java
@@ -1,7 +1,14 @@
 package src;
 
 public class HangManApplication {
+    private static HangManApplication hangManApplication;
 
+    public static HangManApplication getInstance() {
+        if (hangManApplication == null) {
+            hangManApplication = new HangManApplication();
+        }
+        return hangManApplication;
+    }
     public static void main(String[] args) {
 
     }
