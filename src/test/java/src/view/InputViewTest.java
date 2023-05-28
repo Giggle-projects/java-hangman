46b2394da6444d27496da02a4fc4309eb150bccf
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-05-28 16:19:01 +0900

    test: (step1) 게임 횟수, 목숨 입력 기능 테스트 코드 세분화 case1: 입력 값의 콤마로 구분되는 값이 2개가 아닌 경우 case2: 입력 값의 콤마로 구분되는 값이 정수 값이 아닌 경우

--- src/test/java/src/view/InputViewTest.java
+++ src/test/java/src/view/InputViewTest.java
@@ -2,7 +2,7 @@ package src.view;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import src.hangmanGame.HangmanGameInfo;
+import src.hangmanGame.HangmanInfo;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -10,12 +10,34 @@ import java.io.InputStream;
 class InputViewTest {
 
     @Test
-    void inputHangmanGameInfoTest() {
+    void inputHangmanGameInfo_생성_테스트() {
         String input = "1, 4  ";
         InputStream inputStream = new ByteArrayInputStream(input.getBytes());
         System.setIn(inputStream);
 
-        HangmanGameInfo hangmanGameInfo = InputView.inputHangmanGameInfo();
-        Assertions.assertTrue(hangmanGameInfo.equals(new HangmanGameInfo("1", "4")));
+        HangmanInfo hangmanGameInfo = InputView.inputHangmanGameInfo();
+        Assertions.assertTrue(hangmanGameInfo.equals(new HangmanInfo("1", "4")));
+    }
+
+    @Test
+    void inputHangmanGameInfo_콤마로_구분되는_값이_2개가_아닌_경우() {
+        String input = "1, 4, 3";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            HangmanInfo hangmanGameInfo = InputView.inputHangmanGameInfo();
+        });
+    }
+
+    @Test
+    void inputHangmanGameInfo_콤마로_구분되는_값이_정수값이_아닌_경우() {
+        String input = "1, test";
+        InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(inputStream);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            HangmanInfo hangmanGameInfo = InputView.inputHangmanGameInfo();
+        });
     }
 }
