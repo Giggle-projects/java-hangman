ad1a499a1f6e14384250e933a6ea9637e2101ec8
chaewon <djacltk@naver.com>
2023-05-28 21:24:13 +0900

    feat: [step1] 정답 단어 정의, 랜덤 선택 구현

---
+++ src/main/java/src/util/WordBook.java
@@ -0,0 +1,11 @@
+package src.hangman;
+
+import java.util.Random;
+
+public enum WordBook {
+	APPLE, HANGMAN, JAVA, SPRING;
+
+	public static String getRandom(){
+		return values()[new Random().nextInt(WordBook.values().length)].name();
+	}
+}
