abf491d2124fccd3ba9d8bc72dc44a4eb4089936
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-05-28 16:03:37 +0900

    refactor: (step1) RandomWordChooser 인스턴스화 방지

--- src/main/java/src/hangmanGame/RandomWordChooser.java
+++ src/main/java/src/hangmanGame/RandomWordChooser.java
@@ -6,6 +6,9 @@ public class RandomWordChooser {
 
     private static final List<HangmanWord> WORDS = List.of(HangmanWord.values());
 
+    // Suppresses default constructor, ensuring non-instantiability.
+    private RandomWordChooser() { }
+
     public static HangmanWord chooseWord() {
         int randomIndex = (int) (Math.random() * WORDS.size());
         return WORDS.get(randomIndex);
