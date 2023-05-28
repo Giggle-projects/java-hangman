b3d4d01d60749cd76e97943b98baff9a611dad9f
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-05-28 20:10:59 +0900

    refactor: (step1) HangmanGenerator 인스턴스화 방지

--- src/main/java/src/hangmanGame/HangmanGenerator.java
+++ src/main/java/src/hangmanGame/HangmanGenerator.java
@@ -4,6 +4,9 @@ import src.view.InputView;
 
 public class HangmanGenerator {
 
+    // Suppresses default constructor, ensuring non-instantiability.
+    private HangmanGenerator() {}
+
     public static Hangman generate() {
         HangmanInfo info = InputView.inputHangmanGameInfo();
         return new Hangman(info);
