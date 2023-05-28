8d8c478b1aaca8765e569ecdc6ca81e04009d077
chaewon <djacltk@naver.com>
2023-05-28 23:40:42 +0900

    feat: [step1] main 로직 구현

--- src/main/java/src/HangManApplication.java
+++ src/main/java/src/HangManApplication.java
@@ -1,8 +1,26 @@
 package src;
 
+import src.hangman.Hangman;
+import src.hangman.HangmanGame;
+import src.util.InputHelper;
+
 public class HangManApplication {
-
+    private static int playCount;
+    private static int life;
     public static void main(String[] args) {
+        Integer[] inputs=InputHelper.multiIntegerInput("게임 횟수와 목숨을 입력하세요.",", ");
+        playCount=inputs[0];
+        life=inputs[1];
 
+        while (playCount-->0){
+            HangmanGame newGame=new HangmanGame(new Hangman(life));
+
+            boolean result= newGame.start();
+            InputHelper.printInfo(result ? "축하합니다. 정답입니다.\n":"실패입니다. 다시 도전해보세요!\n");
+
+            if(playCount>0){
+                InputHelper.printInfo("다음 게임을 시작합니다.");
+            }
+        }
     }
 }
