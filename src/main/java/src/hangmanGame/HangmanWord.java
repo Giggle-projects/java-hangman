ece6848ef9e62ac7fc0bb9a292a2afd76a933ca2
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-05-28 15:54:13 +0900

    feat: (step1) 미리 정의된 정답을 저장하는 불변 객체 HangmanWord 생성

---
+++ src/main/java/src/hangmanGame/HangmanWord.java
@@ -0,0 +1,19 @@
+package src.hangmanGame;
+
+public enum HangmanWord {
+
+    APPLE("apple"),
+    COMPUTER("computer"),
+    JAVA("java"),
+    HYUNSOO("hyunsoo");
+
+    private final String spelling;
+
+    HangmanWord(String word) {
+        this.spelling = word;
+    }
+
+    public String getSpelling() {
+        return spelling;
+    }
+}
