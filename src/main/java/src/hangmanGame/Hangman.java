3703a1d7c8a05ee9aeffbaacb68e53f5fd4e87bb
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-05-29 21:09:02 +0900

    refactor: (step4) Hangman 객체에서 HangmanGame 객체로 게임 라운드에 대한 정보를 저장하는 테이블을 넘겨주도록 변경 기존 HangmanGame의 생성자에서 HangmanGameRoundTable의 메모리를 할당하는 방식에서 의존성 생성자 주입으로 변경

--- src/main/java/src/hangmanGame/Hangman.java
+++ src/main/java/src/hangmanGame/Hangman.java
@@ -1,5 +1,6 @@
 package src.hangmanGame;
 
+import src.view.InputView;
 import src.view.OutputView;
 
 import java.util.Objects;
@@ -15,57 +16,72 @@ public class Hangman {
     private static final int MAX_LIFE = 1000;
     private static final int MIN_LIFE = 1;
 
-    private final int numberGames;
-    private final int life;
+    private final HangmanInfo hangmanInfo;
+    private final HangmanGameTable gameTable;
 
-    public Hangman(int numberGames, int life) {
-        validateNumberGames(numberGames);
-        validateLife(life);
-        this.numberGames = numberGames;
-        this.life = life;
+    public Hangman(HangmanInfo hangmanInfo, HangmanGameTable gameTable) {
+        this.hangmanInfo = hangmanInfo;
+        this.gameTable = gameTable;
     }
 
     public void gameSet() {
         HangmanGame hangmanGame;
 
-        for (int gameCount = 0; gameCount < numberGames; gameCount++) {
-            hangmanGame = initHangmanGame(gameCount);
+        for (int gameCount = 0; gameCount < hangmanInfo.numberGames; gameCount++) {
+            hangmanGame = generateHangmanGame(gameCount);
             hangmanGame.start();
             if (!isLastGame(gameCount)) {
                 OutputView.printMessage(GAME_NEXT_MESSAGE);
             }
         }
+
         OutputView.printMessage(GAME_END_MESSAGE);
     }
 
-    private HangmanGame initHangmanGame(int gameCount) {
+    private HangmanGame generateHangmanGame(int gameCount) {
         int numberGames = gameCount + 1;
-        int life = this.life;
+        int life = hangmanInfo.life;
         HangmanWord randomWord = RandomWordChooser.chooseWord();
-        HangmanGameRoundTable roundTable = new HangmanGameRoundTable();
+        HangmanGameRoundTable roundTable = gameTable.getRoundTable(numberGames);
 
         return new HangmanGame(numberGames, life, randomWord, roundTable);
     }
 
-    private boolean isLastGame(int gameCount ) {
-        return gameCount == numberGames - 1;
-    }
-
-
-    private void validateNumberGames(int numberGames) throws IllegalArgumentException {
-        if (MIN_NUMBER_GAMES > numberGames || MAX_NUMBER_GAMES < numberGames) {
-            throw new IllegalArgumentException("게임 횟수 범위 밖의 숫자입니다.");
-        }
-    }
-
-    private void validateLife(int life) throws IllegalArgumentException {
-        if (MIN_LIFE > life || MAX_LIFE < life) {
-            throw new IllegalArgumentException("목숨 횟수 범위 밖의 숫자입니다.");
-        }
+    private boolean isLastGame(int gameCount) {
+        return gameCount == hangmanInfo.numberGames - 1;
     }
 
     public boolean equals(Hangman that) {
-        return Objects.equals(numberGames, that.numberGames)
-                && Objects.equals(life, that.life);
+        return this.hangmanInfo.equals(that.hangmanInfo)
+                && Objects.equals(this.gameTable, that.gameTable);
+    }
+
+    public static class HangmanInfo {
+        public final int numberGames;
+        public final int life;
+
+        public HangmanInfo(int numberGames, int life) {
+            validateNumberGames(numberGames);
+            validateLife(life);
+            this.numberGames = numberGames;
+            this.life = life;
+        }
+
+        private void validateNumberGames(int numberGames) throws IllegalArgumentException {
+            if (MIN_NUMBER_GAMES > numberGames || MAX_NUMBER_GAMES < numberGames) {
+                throw new IllegalArgumentException("게임 횟수 범위 밖의 숫자입니다.");
+            }
+        }
+
+        private void validateLife(int life) throws IllegalArgumentException {
+            if (MIN_LIFE > life || MAX_LIFE < life) {
+                throw new IllegalArgumentException("목숨 횟수 범위 밖의 숫자입니다.");
+            }
+        }
+
+        public boolean equals(HangmanInfo that) {
+            return Objects.equals(numberGames, that.numberGames)
+                    && Objects.equals(life, that.life);
+        }
     }
 }
