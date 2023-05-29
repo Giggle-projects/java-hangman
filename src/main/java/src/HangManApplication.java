1543dc0fa954cfbf961f6b4c33cb0d12ac413576
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-05-29 21:07:13 +0900

    refactor: (step4) 메인 메서드에서 전체 행맨 게임을 생성하도록 변경 기능 추가로 인한 각 클래스 멤버의 증가로 Hangman 객체 생성 코드의 복잡도 증가. 이로 인해 정적 팩토리 메서드로 분리하여 hangman 객체를 생성하도록 변경.

--- src/main/java/src/HangManApplication.java
+++ src/main/java/src/HangManApplication.java
@@ -1,14 +1,28 @@
 package src;
 
 import src.hangmanGame.Hangman;
+import src.hangmanGame.HangmanGameRoundTable;
+import src.hangmanGame.HangmanGameTable;
 import src.util.Console;
 import src.view.InputView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class HangManApplication {
 
     public static void main(String[] args) {
-        Hangman hangman = InputView.inputHangmanInfo();
+        Hangman hangman = generateHangman();
         hangman.gameSet();
         Console.close();
+    }
+
+    public static Hangman generateHangman() {
+        Hangman.HangmanInfo hangmanInfo = InputView.inputHangmanInfo();
+        Map<Integer, HangmanGameRoundTable> hangmanGameRoundTableMap = new HashMap<>();
+        for (int i = 0; i < hangmanInfo.numberGames; i++) {
+            hangmanGameRoundTableMap.put(i+1, new HangmanGameRoundTable(new HashMap<>()));
+        }
+        return new Hangman(hangmanInfo, new HangmanGameTable(hangmanGameRoundTableMap));
     }
 }
