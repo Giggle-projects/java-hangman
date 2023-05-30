f2e76cd60491c1a5dd5c695452f255feabca4cd7
chaewon <djacltk@naver.com>
2023-05-30 23:03:40 +0900

    feat: [step4] 메뉴 선택 로직 구현

--- src/main/java/src/HangManApplication.java
+++ src/main/java/src/HangManApplication.java
@@ -2,17 +2,85 @@ package src;
 
 import static src.exception.ErrorCode.*;
 
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import src.exception.InvalidInputFormatException;
 import src.hangman.GameLauncher;
-import src.hangman.Hangman;
-import src.hangman.HangmanGame;
 import src.util.InputHelper;
 
 public class HangManApplication {
-
     private static int playCount;
     private static int life;
     public static void main(String[] args) {
+        while (true){
+            int inputs = InputHelper.singleIntegerInput(RootMenu.chooseDescription());
+            RootMenu rootMenu=RootMenu.findByCode(inputs);
+            rootMenu.run();
+
+            if(rootMenu == RootMenu.END){
+                break;
+            }
+        }
+    }
+
+    private enum RootMenu{
+        PLAY_GAME("게임하기",1,HangManApplication::playGame),
+        SHOW_GAME_RESULT("게임 결과 보기",2,HangManApplication::showGameResult),
+        SHOW_ROUND_RESULT("라운드 결과 보기",3,HangManApplication::showRoundResult),
+        END("종료",4,() -> {
+            InputHelper.printInfo("프로그램을 종료합니다.");
+            return null;
+        });
+
+        String description;
+        Integer code;
+        Supplier<Void> function;
+
+        RootMenu(String description, Integer code, Supplier<Void> function) {
+            this.description = description;
+            this.code = code;
+            this.function = function;
+        }
+
+        public Void run(){
+            return function.get();
+        }
+
+        public static RootMenu findByCode(int code){
+            return Arrays.stream(RootMenu.values())
+                .filter(m -> m.getCode() == code)
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
+        }
+        public static String chooseDescription(){
+            StringBuilder sb = new StringBuilder();
+            sb.append("메뉴를 선택합니다. (");
+            sb.append(
+                Arrays.stream(RootMenu.values())
+                    .map(RootMenu::toString)
+                    .collect(Collectors.joining(", "))
+                    .toString()
+            );
+            sb.append(")");
+
+            return sb.toString();
+        }
+
+        public Integer getCode() {
+            return code;
+        }
+
+        @Override
+        public String toString() {
+            return code + " : " + description  ;
+        }
+    }
+
+    private static Void playGame() {
         while (true){
             try{
                 Integer[] inputs = InputHelper.multiIntegerInput("게임 횟수와 목숨을 입력하세요.");
@@ -36,5 +104,14 @@ public class HangManApplication {
                 InputHelper.printInfo("다음 게임을 시작합니다.");
             }
         }
+
+        return null;
+    }
+    private static Void showGameResult() {
+        return null;
+    }
+
+    private static Void showRoundResult() {
+        return null;
     }
 }
