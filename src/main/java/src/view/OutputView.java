4a5ef22969befecb35ed21f331628bde65a7502d
hyunsb <96504592+hyunsb@users.noreply.github.com>
2023-05-30 21:24:22 +0900

    refactor: 게임 결과 출력을 OutputView 클래스가 담당하도록 수정

--- src/main/java/src/view/OutputView.java
+++ src/main/java/src/view/OutputView.java
@@ -1,13 +1,35 @@
 package src.view;
 
+import src.hangmanGame.HangmanGameRoundTable;
+
+import java.util.Iterator;
+
 public class OutputView {
 
     private static final String OU = "ou : ";
+
+    private static final String ROUND_PRINT_FORMAT = "라운드 id: %d, 남은 목숨 : %d, %s, 사용자 입력 : %c\n";
+
 
     // Suppresses default constructor, ensuring non-instantiability.
     private OutputView() {}
 
     public static void printMessage(String message) {
         System.out.println(OU + message);
+    }
+
+    public static void printGameResult(HangmanGameRoundTable roundTable) {
+        StringBuilder result = new StringBuilder();
+        result.append("\n=== Game Result ===\n").append(roundTable.gameResult());
+        for (Integer roundId : roundTable) {
+            HangmanGameRoundTable.HangmanGameRoundInfo round = roundTable.getRound(roundId);
+            result.append(String.format(ROUND_PRINT_FORMAT
+                    , roundId, round.remainingLife, round.correctingWord, round.inputAlphabet));
+        }
+        printMessage(result.append("===================\n").toString());
+    }
+
+    public static void printRoundResult() {
+
     }
 }
