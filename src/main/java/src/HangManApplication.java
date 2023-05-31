73889bd00859acab575a88d1ae61fb4e75086bda
yeeun <dpdmstjs@gmail.com>
2023-05-31 20:54:43 +0900

    step1 : 게임 횟수, 목숨 횟수 입력 구현

--- src/main/java/src/HangManApplication.java
+++ src/main/java/src/HangManApplication.java
@@ -1,8 +1,15 @@
 package src;
 
-public class HangManApplication {
+import java.util.Scanner;
 
+public class HangManApplication {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] inputs = input.split(",");
+        int numberGames = Integer.parseInt(inputs[0]);
+        int numberLives = Integer.parseInt(inputs[1]);
 
     }
 }
