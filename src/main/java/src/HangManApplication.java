e3e06efb171da15e5c9d80ee3665ea27c328729f
yeeun <dpdmstjs@gmail.com>
2023-05-31 21:00:16 +0900

    step1 : 정답 단어 랜덤으로 선택되게 구현

--- src/main/java/src/HangManApplication.java
+++ src/main/java/src/HangManApplication.java
@@ -1,9 +1,21 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 public class HangManApplication {
+
     public static void main(String[] args) {
+
+        List<String> wordList = new ArrayList<>();
+        wordList.add("apple");
+        wordList.add("strawberry");
+        wordList.add("grape");
+
+        Random random = new Random();
+        String word = wordList.get(random.nextInt(wordList.size()));
 
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
