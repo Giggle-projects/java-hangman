3133dac22d3f4f90ddcd6871e7dd36d5753af7e0
inyoung <charmingyear@gmail.com>
2023-06-02 01:08:05 +0900

    step2 : 예외처리하기

--- src/main/java/src/Play.java
+++ src/main/java/src/Play.java
@@ -1,6 +1,5 @@
 package src;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Play{
@@ -9,9 +8,26 @@ public class Play{
         User user = new User();
         System.out.println("게임 횟수와 목숨을 입력하세요.");
         Scanner scanner = new Scanner(System.in);
-        String[] input = scanner.nextLine().split(",");
-        user.setGameNum(Integer.parseInt(input[0].trim()));
-        user.setLife(Integer.parseInt(input[1].trim()));
+
+        boolean isValidInput = false;
+
+        while (!isValidInput){
+            try {
+                String[] input = scanner.nextLine().split(",");
+                if (input.length < 2){
+                    throw new IllegalArgumentException("숫자, 숫자 형태로 입력해주세요.");
+                }
+                int gameNum = Integer.parseInt(input[0].trim());
+                int life = Integer.parseInt(input[1].trim());
+                user.setGameNum(gameNum);
+                user.setLife(life);
+                isValidInput = true;
+            } catch (NumberFormatException e){
+                System.out.println("숫자, 숫자 형태로 입력해주세요.");
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
 
         int userLife = user.getLife();
         for (int i = 0; i < user.getGameNum(); i++){
@@ -27,8 +43,8 @@ public class Play{
     }
 
     private static void round(int userLife, String answer, char[] blind, Scanner scanner) {
+        String blindScreen = String.valueOf(blind);
         for (int i = 1; userLife > 0; i++){
-            String blindScreen = String.valueOf(blind);
             System.out.println(i + " 라운드 : " + blindScreen + ", 목숨 " + userLife);
             boolean isValid = true;
             char ch = '\0';
@@ -62,6 +78,7 @@ public class Play{
                     break;
                 }
             }
+            
             if (isBlind){
                 System.out.println("축하합니다. 정답입니다.");
                 break;
@@ -71,4 +88,5 @@ public class Play{
             }
         }
     }
+
 }
