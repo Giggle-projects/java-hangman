f17ba70737f5c375e4dcd6658175d11376c87d90
ussu1112 <ussu1112@naver.com>
2023-06-01 15:56:20 +0900

    fix: 문제 오류로 인해 정답 입력시에 답 확인이 안되는 버그 수정

--- src/main/java/src/example/Animal.java
+++ src/main/java/src/example/Animal.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public enum Animal implements Example {
 
-    CAT("cat"), FROG("frog"), CHICKEN("chicken"), turtle("turtle"), crab("CRAB"),
+    CAT("cat"), FROG("frog"), CHICKEN("chicken"), TURTLE("turtle"), CRAB("crab"),
     RABBIT("rabbit"), SHARK("shark"), CROCODILE("crocodile"), GIRAFFE("giraffe"), COW("cow"),
     HORSE("horse"), BUTTERFLY("butterfly"), BULL("bull"), PIG("pig"), RHINO("rhino"),
     SHEEP("sheep"), SNAKE("snake"), PANDA("panda"), EAGLE("eagle"), SWAN("swan");
