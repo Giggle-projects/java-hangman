82e0921ddaf66d3220519a23b5e4e45e43d0fc10
sdfgx123 <sdfgx123@naver.com>
2023-06-02 16:04:57 +0900

    feat: step 2 메뉴 선택 범위 이탈 예외 처리 추가

--- src/main/java/src/Input.java
+++ src/main/java/src/Input.java
@@ -1,6 +1,7 @@
 package src;
 
 import src.exception.IllegalInputLengthException;
+import src.exception.InputMenuException;
 import src.util.Message;
 
 import java.util.Scanner;
@@ -55,10 +56,15 @@ public class Input {
                     Integer numOfRoundId = Integer.parseInt(input);
                     Game.findRoundById(numOfRoundId);
                     chooseMenu();
+                } else {
+                    throw new InputMenuException(Message.ERR_OUT_OF_MENU_BOUNDARY);
                 }
 
             } catch (NumberFormatException e) {
                 System.out.println(Message.ERR_NUMBER_FORMAT_EXCEPTION);
+                chooseMenu();
+            } catch (InputMenuException e) {
+                System.out.println(Message.ERR_OUT_OF_MENU_BOUNDARY);
                 chooseMenu();
             } catch (Exception e) {
                 System.out.println(Message.ERR_UNKNOWN_ERROR);
@@ -67,34 +73,25 @@ public class Input {
         }
     }
 
-    /**
-     * 게임 초기 설정값을 받을 때 사용할 메서드
-     * @return
-     */
     public static int[] getInitiationParam() {
         try {
             int[] res = new int[2];
-            // 이거 next()로 받으면 예외 터짐
             String input = scanner.nextLine();
             String[] splittedArr = input.split(",");
 
             if (splittedArr.length != 2) {
-                throw new IllegalArgumentException("두 개의 숫자를 콤마로 구분하여 입력하시오.");
+                throw new IllegalArgumentException(Message.ERR_NOT_USING_COMMA);
             }
 
             res[0] = Integer.parseInt(splittedArr[0].trim());
             res[1] = Integer.parseInt(splittedArr[1].trim());
             return res;
-        } catch (Exception e) {
+        } catch (IllegalInputLengthException e) {
             System.out.println(e.getMessage());
             return getInitiationParam();
         }
     }
 
-    /**
-     * 게임 중 계속해서 값을 받을 때 사용할 메서드
-     * @return
-     */
     public static char getGameParam() {
         try {
             String str = scanner.next();
--- src/main/java/src/util/Message.java
+++ src/main/java/src/util/Message.java
@@ -5,4 +5,6 @@ public interface Message {
     String ERR_INPUT_RANGE = "You just typed more than one character. Please retype one character.";
     String ERR_NUMBER_FORMAT_EXCEPTION = "The letter you just typed is not a digit. Please retype the digit.";
     String ERR_UNKNOWN_ERROR = "Unknown error occured.";
+    String ERR_NOT_USING_COMMA = "The letter you just typed is not separated with comma. Please retype using comma.";
+    String ERR_OUT_OF_MENU_BOUNDARY = "Your choice is wrong. Please choose valid menu category.";
 }
