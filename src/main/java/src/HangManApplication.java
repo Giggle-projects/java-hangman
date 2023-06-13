10c92ef1fc05a17c42e9bc3379cd0f489ac45410
inyoung <charmingyear@gmail.com>
2023-06-13 16:31:55 +0900

    step7 : try-catch-resource 구문 활용

--- src/main/java/src/HangManApplication.java
+++ src/main/java/src/HangManApplication.java
@@ -3,40 +3,40 @@ package src;
 import java.util.*;
 
 public class HangManApplication {
-    private static final Scanner scanner = new Scanner(System.in);
-    private static final User user = new User();
     private static final List<Game> games = new ArrayList<>();
 
     public static void main(String[] args) {
-        while (true) {
-            System.out.println("===================");
-            System.out.println("메뉴를 선택합니다. (1 : 게임하기, 2 : 게임 결과 보기, 3 : 라운드 결과 보기, 4 : 게임종료)");
-            if (scanner.hasNextInt()) {
-                int menuNum = scanner.nextInt();
-                switch (menuNum) {
-                    case 1 -> playGame();
-                    case 2 -> showGameResult();
-                    case 3 -> showRoundResult();
-                    case 4 -> {
-                        System.out.println("종료되었습니다.");
-                        return;
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("===================");
+                System.out.println("메뉴를 선택합니다. (1 : 게임하기, 2 : 게임 결과 보기, 3 : 라운드 결과 보기, 4 : 게임종료)");
+                if (scanner.hasNextInt()) {
+                    int menuNum = scanner.nextInt();
+                    switch (menuNum) {
+                        case 1 -> playGame(scanner);
+                        case 2 -> showGameResult();
+                        case 3 -> showRoundResult();
+                        case 4 -> {
+                            System.out.println("종료되었습니다.");
+                            return;
+                        }
+                        default -> System.out.println("잘못된 번호입니다.");
                     }
-                    default -> System.out.println("잘못된 번호입니다.");
+                } else {
+                    System.out.println("숫자를 입력해주세요.");
+                    scanner.next();
                 }
-            } else {
-                System.out.println("숫자를 입력해주세요.");
-                scanner.next();
             }
         }
     }
 
-    public static void playGame() {
+    public static void playGame(Scanner scanner) {
         boolean isValidInput = false;
         int gameId = 1;
+        scanner.nextLine(); // 개행문자 제거
         while (!isValidInput) {
             try {
                 System.out.println("게임 횟수와 목숨을 입력하세요.");
-                scanner.nextLine();
                 String str = scanner.nextLine();
                 String[] input = str.split(",");
                 if (input.length < 2) {
@@ -44,30 +44,30 @@ public class HangManApplication {
                 }
                 int gameNum = Integer.parseInt(input[0].trim());
                 int life = Integer.parseInt(input[1].trim());
+                User user = new User();
                 user.setGameNum(gameNum);
                 user.setLife(life);
                 isValidInput = true;
+                int userLife = user.getLife();
+                for (int i = 0; i < user.getGameNum(); i++) {
+                    String answer = GameAnswer.setAnswer();
+                    char[] blind = GameAnswer.setBlind(answer);
+                    if (i != 0) {
+                        System.out.println("\n다음 게임을 시작합니다.");
+                    }
+                    System.out.println(gameId + "번째 게임이 시작됩니다. 정답 단어는 " + answer.length() + "글자 입니다.");
+                    playRound(gameId, userLife, answer, blind, scanner);
+                    gameId++;
+                }
             } catch (NumberFormatException e) {
                 System.out.println("숫자, 숫자 형태로 입력해주세요.");
             } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
             }
         }
-
-        int userLife = user.getLife();
-        for (int i = 0; i < user.getGameNum(); i++) {
-            String answer = GameAnswer.setAnswer();
-            char[] blind = GameAnswer.setBlind(answer);
-            if (i != 0) {
-                System.out.println("\n다음 게임을 시작합니다.");
-            }
-            System.out.println(gameId + "번째 게임이 시작됩니다. 정답 단어는 " + answer.length() + "글자 입니다.");
-            playRound(gameId, userLife, answer, blind);
-            gameId++;
-        }
     }
 
-    private static void playRound(int gameId, int userLife, String answer, char[] blind) {
+    private static void playRound(int gameId, int userLife, String answer, char[] blind, Scanner scanner) {
         int roundId = 1;
         Set<Character> prevAlp = new HashSet<>();
         List<Round> gameRound = new ArrayList<>();
@@ -76,7 +76,7 @@ public class HangManApplication {
             String blindScreen = String.valueOf(blind);
             System.out.println(roundId + " 라운드 : " + blindScreen + ", 목숨 " + userLife);
 
-            char guess = getValidAlphabetInput();
+            char guess = getValidAlphabetInput(scanner);
             guess = Character.toLowerCase(guess);
 
             if (prevAlp.contains(guess)) {
@@ -115,7 +115,7 @@ public class HangManApplication {
         GameResult(gameId);
     }
 
-    private static boolean isSuccess(char[] blind){
+    private static boolean isSuccess(char[] blind) {
         for (char c : blind) {
             if (c == '_') {
                 return false;
@@ -151,37 +151,42 @@ public class HangManApplication {
     }
 
     public static void showGameResult() {
-        int gameId = getValidInput("게임 ID를 입력하세요: ");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("게임 ID를 입력하세요.");
+            int gameId = getValidInput(scanner, "게임 ID를 입력하세요: ");
 
-        if (gameId <= 0 || gameId > games.size()) {
-            System.out.println("해당 결과가 존재하지 않습니다.");
-        } else {
-            GameResult(gameId);
+            if (gameId <= 0 || gameId > games.size()) {
+                System.out.println("해당 결과가 존재하지 않습니다.");
+            } else {
+                GameResult(gameId);
+            }
         }
     }
 
     public static void showRoundResult() {
-        System.out.println("게임 ID를 입력하세요.");
-        int gameId = getValidInput("게임 ID를 입력하세요: ");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("게임 ID를 입력하세요.");
+            int gameId = getValidInput(scanner, "게임 ID를 입력하세요: ");
 
-        if (gameId <= 0 || gameId > games.size()) {
-            System.out.println("해당 결과가 존재하지 않습니다.");
-            return;
-        }
+            if (gameId <= 0 || gameId > games.size()) {
+                System.out.println("해당 결과가 존재하지 않습니다.");
+                return;
+            }
 
-        int roundId = getValidInput("라운드 ID를 입력하세요: ");
+            int roundId = getValidInput(scanner, "라운드 ID를 입력하세요: ");
 
-        Game game = games.get(gameId - 1);
-        List<Round> roundList = game.getRounds();
+            Game game = games.get(gameId - 1);
+            List<Round> roundList = game.getRounds();
 
-        if (roundId <= 0 || roundId > roundList.size()) {
-            System.out.println("해당 결과가 존재하지 않습니다.");
-        } else {
-            RoundResult(gameId, roundId);
+            if (roundId <= 0 || roundId > roundList.size()) {
+                System.out.println("해당 결과가 존재하지 않습니다.");
+            } else {
+                RoundResult(gameId, roundId);
+            }
         }
     }
 
-    private static int getValidInput(String msg) {
+    private static int getValidInput(Scanner scanner, String msg) {
         while (true) {
             try {
                 System.out.print(msg);
@@ -195,7 +200,7 @@ public class HangManApplication {
         }
     }
 
-    private static char getValidAlphabetInput() {
+    private static char getValidAlphabetInput(Scanner scanner) {
         while (true) {
             try {
                 System.out.print("알파벳을 입력해주세요.");
