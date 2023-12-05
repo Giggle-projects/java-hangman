453ee3f984cbc8bb8b23cca432a5c818e1b33880
hyunsb <junghss0113@gmail.com>
2023-12-06 00:35:45 +0900

    feat: 게임 목숨을 입력받는 기능 구현

---
+++ src/main/java/domain/Life.java
@@ -0,0 +1,27 @@
+package domain;
+
+public class Life {
+
+    private final int life;
+    private int remainingLife;
+
+    public Life(int life) {
+        this.validate(life);
+        this.life = life;
+        this.remainingLife = life;
+    }
+
+    private void validate(int life) {
+        if (life < 1) {
+            throw new IllegalArgumentException("목숨은 1 이상이어야 합니다.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Life{" +
+                "life=" + life +
+                ", remainingLife=" + remainingLife +
+                '}';
+    }
+}
